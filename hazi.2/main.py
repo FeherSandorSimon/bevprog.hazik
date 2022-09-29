0f070376982c6767e8cd30c314b5100107ebd8c9
FeherSandorSimon <fehersandorsimon@gmail.com>
2022-09-29 14:29:06 +0200

    Házi

---
+++ hazi.2/main.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    print(" Adja meg a háromszög három oldalát cm-ben: ")
+    a = int(input(" a oldal (cm): "))
+    b = int(input(" b oldal (cm): "))
+    c = int(input(" c oldal (cm): "))
+
+    if a + b > c and a + c > b and b + c > a:
+        print(" A " + str(a) + ", " + str(b) + " és " + str(c) + " oldalú háromszög szerkeszthető. ")
+    else:
+        print(" A " + str(a) + ", " + str(b) + " és " + str(c) + " oldalú háromszög NEM szerkeszthető. ")
